8a20056045aa056197728e474cf74765e44e8308
Trevor Westphal <twestpha@mines.edu>
2015-06-02 20:05:37 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# main.py
+# Trevor Westphal
+
+import datetime
+from parsing_lib.brothersbrick import parseBrothersBrick
+
+class parseController:
+
+	def __init__(self):
+		self.setupKeywordParsingDict()
+
+	def setupKeywordParsingDict(self):
+		self.parsingDict = {
+			"brothersbrick":parseBrothersBrick,
+		}
+
+	def parse(self, array):
+		try:
+			self.parsingDict[array[1]](array[0], array[2], array[3])
+		except:
+			of = open("error_log.txt", "a")
+			of.write("Error: %s, %s, %s, %s\n" % (array[0], array[1], array[2], datetime.datetime.now()))
+
+# Entry point
+p = parseController()
+f = open("sourcelist.txt", "r")
+
+for line in f:
+	line_array = line.strip().split("|")
+	if(line[0] != "#"):
+		p.parse(line_array)
+
+f.close()
---
+++ parsing_lib/brothersbrick.py
@@ -0,0 +1,7 @@
+# brothersbrick.py
+# Trevor Westphal
+
+def parseBrothersBrick(source, url, category):
+	print("Source:%s, URL:%s, Category:%s" % (source, url, category))
+
+	# First, open the db metadata
